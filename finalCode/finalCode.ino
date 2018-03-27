2fccdde4045ec304f805c450a18287782f0d99ad
Mykhaylo Glushakov <mg367@duke.edu>
2018-03-27 15:32:51 -0400

    small changes

--- finalCode/finalCode.ino
+++ finalCode/finalCode.ino
@@ -1,56 +1,59 @@
-#define  Rx 15
+#define Rx 15
 #define Tx 14
-#include <Servo.h>                           // Include servo library
+#include <Servo.h>
+
 // QTI Threshold
 long t = 500;
+
 // Pause variable
-bool pausedForBlack = false;
-bool lineFollowB,getCourtB,bsendValsB,danceB,singB,lightShowB;
+//bool pausedForBlack = false;
+
+// Booleans for order of operations
+bool lineFollowB, getCourtB, sendValsB, danceB, singB, lightShowB;
 
 // Declare left and right servos
 Servo servoLeft;
 Servo servoRight;
 
 void setup() {
-  //Servo Setup
+  // Servo Setup
   servoLeft.attach(13);
   servoRight.attach(12);
-//QTI Setup
+
+  // QTI Setup
   pinMode(4, INPUT);
   pinMode(5, INPUT);
-  pinMode(6, INPUT);  
-//Serial Setup
+  pinMode(6, INPUT);
+
+  // Serial Setup
   Serial.begin(9600);
   Serial3.begin(9600);
-  
+
+  // Booleans for order of operations
+  lineFollowB = true;
+  getCourtB = false;
+  sendValsB = false;
+  danceB = false;
+  singB = false;
+  lightShowB = false;
+
   delay(500);
-//booleans for order of operations:
-lineFollowB=true;
-getCourtB=false;
-sendValsB=false;
-danceB=false;
-sing=false;
-lightShowB=false;
-
-  
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(lineFollowB)
+  if (lineFollowB)
   {
-    lineFollow(rcTime(4),rcTime(5),rcTime(6));  
+    lineFollow(rcTime(4), rcTime(5), rcTime(6));
   }
-  else if(getCourtB)
+  else if (getCourtB)
   {
-    getCourt();
-    }
-    //Each boolean correspons to the task in progress, once the task is done the boolean
-    //is switched to false and the subsequent boolesn is switched to true
-  
-
+    int courtType = getCourt();
+    //getCourt();
+  }
+  //Each boolean correspons to the task in progress, once the task is done the boolean
+  //is switched to false and the subsequent boolesn is switched to true
 }
+
 //______________________________________________
 //HELPER FUNCTIONS
 //______________________________________________
@@ -72,58 +75,67 @@ long rcTime(int pin) {
 //SERVO HELPERS
 void goForward()
 {
-    servoLeft.writeMicroseconds(1550);
-    servoRight.writeMicroseconds(1450);  
-}
-void goLeft()
-{
-   servoLeft.writeMicroseconds(1650);
-   servoRight.writeMicroseconds(1550);
+  servoLeft.writeMicroseconds(1550);
+  servoRight.writeMicroseconds(1450);
 }
 void goRight()
 {
+  servoLeft.writeMicroseconds(1650);
+  servoRight.writeMicroseconds(1550);
+}
+void goLeft()
+{
   servoLeft.writeMicroseconds(1450);
-  servoRight.writeMicroseconds(1350);  
+  servoRight.writeMicroseconds(1350);
 }
 void stopMotors()
 {
   servoLeft.writeMicroseconds(1500);
-  servoRight.writeMicroseconds(1500);  
+  servoRight.writeMicroseconds(1500);
 }
 //________________________________________________
 //LINE FOLLOW
-void lineFollow(long qtiRight,long qtiMiddle, long qtiLeft)
+void lineFollow(long qtiRight, long qtiMiddle, long qtiLeft)
 {
   //if (qtiMiddle > t) { // If center qti on black, start movement
   if (qtiRight < t && qtiLeft < t) { // If side qtis are on white, move forward
     goForward();
   }
   if (qtiRight > t && qtiLeft < t) { // If right sees white and left sees black, turn left
-    goLeft();
-  }
-  if (qtiRight < t && qtiLeft > t) { // If right sees black and left sees white, turn right
     goRight();
   }
+  if (qtiRight < t && qtiLeft > t) { // If right sees black and left sees white, turn right
+    goLeft();
+  }
   if (qtiRight > t && qtiLeft > t) { // If both see black, stop, switch getCourtB to True switch lineFollowB to False
-    
-     stopMotors();
-      delay(200);
-      pausedForBlack = true;
-      lineFollowB=false;
-      getCourtB=true;
-  
+
+    stopMotors();
+    delay(500);
+    //pausedForBlack = true;
+    lineFollowB = false;
+    getCourtB = true;
+
   }
   else {
-    pausedForBlack = false;
+    //pausedForBlack = false;
   }
   //}
-}
 }
 //________________________________________________
 //Get Court Type
 int getCourt()
 {
-  //Fill In Later
+  // Turn left 90 degrees in place
+  goLeft();
+  delay(900);
+  goForward();
+  delay(2500);
+  stopMotors();
+
+  
+
+  getCourtB = false;
+  return 0;
 }
 //________________________________________________
 //DEBUG VIA SERIAL
