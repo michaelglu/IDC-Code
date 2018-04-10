ae85367129ecdd9127ea5528cb2523b0f6a876f9
Samuel Rabinowitz <srabinowitz9@gmail.com>
2018-04-10 00:13:33 -0400

    finished IDC 4

--- finalCode/finalCode.ino
+++ finalCode/finalCode.ino
@@ -8,7 +8,6 @@ const int middleQTIPin = 8;
 const int leftQTIPin = 6;
 
 const int commLedPin = 9;
-const int recLedPin = 11;
 
 // QTI Threshold
 long t = 500;
@@ -19,10 +18,41 @@ long blackC = 5000;
 //bool pausedForBlack = false;
 
 // Booleans for order of operations
-bool lineFollowB, getCourtB, sendCourtB;
+bool lineFollowB, getCourtB, communicationB, flashMyScoreB;
 
-int courtType = -1;
-int score = 0;
+// Communication
+const int DIVING = 0; // DIVING: a, b, and c
+const int WEIGHTLIFTING = 1; // WEIGHTLIFTING: d, e, and f
+const int CYCLING = 2; // CYCLING: g, h, i
+const int BASKETBALL = 3; // BASKETBALL: j, k, l
+const int COURT_SPORTS = 4; // COURT_SPORTS: m, n, o
+const String letters = "abcdefghijklmno";
+
+int myTeam = COURT_SPORTS; // PUT YOUR TEAM HERE!!!
+int myScore = -1; // SET THIS TO YOUR SCORE OF 0, 1, OR 2 BASED ON SENSING!!!
+int received[5] = { -1, -1, -1, -1, -1 };
+int finalScore = -1;
+
+// Lightshow
+int greenpin = 9;
+int redpin = 10;
+int yellowpin = 4;
+
+// National Anthem
+int piezoPin = 9;
+#include "pitches.h"
+int melodyBass[] = {
+  G2, F2, E2, F2, G2, A2, B2, C3, D3,
+  G2, F2, B2, F3, B3, AS2, A2, A2, D2, B2, D3, FS3,
+  D3, D3, D3, C3, B2, FS2, G2, G2, C3, B2, A2, G2,
+  D2, B2, B1, C2, A1, B1, C2, D2, D2, G2
+};
+int noteDurations5[] = {
+  2, 2, 3, 1, 1, 1, 1, 1, 4,
+  2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  4, 2, 2, 1, 1, 1, 2, 2, 4
+};
 
 // Declare left and right servos
 Servo servoLeft;
@@ -41,8 +71,13 @@ void setup() {
   // Communication LED setup
   pinMode(commLedPin, OUTPUT);
 
-  // Receiving LED setup
-  pinMode(recLedPin, OUTPUT);
+  // Lightshow setup
+  pinMode(greenpin, OUTPUT);     //Initialize the LED pins as outputs
+  pinMode(redpin, OUTPUT);     //Initialize the LED pins as outputs
+  pinMode(yellowpin, OUTPUT);     //Initialize the LED pins as outputs
+  digitalWrite(greenpin, LOW);  //turn the sent light off
+  digitalWrite(redpin, LOW);  //turn the sent light off
+  digitalWrite(yellowpin, LOW);  //turn the sent light off
 
   // Serial setup
   Serial.begin(9600);
@@ -58,17 +93,45 @@ void loop() {
   // Each boolean correspons to the task in progress, once the task is done the boolean
   // is switched to false and the subsequent boolesn is switched to true
 
-  if (lineFollowB)
-  {
+  if (lineFollowB) {
     lineFollow(rcTime(rightQTIPin), rcTime(middleQTIPin), rcTime(leftQTIPin));
   }
-  if (getCourtB)
-  {
-    courtType = getCourt();
+  if (getCourtB) {
+    myScore = getCourt();
   }
-  else if (sendCourtB) {
-    sendCourt(courtType);
-    receive();
+  if (communicationB) {
+    // Set own score in received
+    received[myTeam] = myScore;
+
+    // Communicate until all scores are in and calculated
+    while (finalScore == -1) {
+      finalScore = communication();
+      flashMyScore(myScore);
+    }
+
+    Serial.println(finalScore);
+
+    // Final action - PUT YOUR ACTIONS UNDER HERE
+    switch (finalScore) {
+      case 0:
+        // Bronze - coordinated dance
+        dance();
+        break;
+      case 1:
+        // Silver - light show
+        lightshowz();
+        break;
+      case 2:
+        // Gold - sing national anthem
+        playAnthem();
+        break;
+    }
+
+    communicationB = false;
+    flashMyScoreB = true;
+  }
+  if (flashMyScoreB) {
+    flashMyScore(myScore);
   }
 }
 
@@ -195,13 +258,13 @@ int getCourt()
   Serial.println(ave);
 
   getCourtB = false;
-  sendCourtB = true;
+  communicationB = true;
   return ave;
 }
 
 // -----------------------------------------------
-// Send value of court and flash communication LED
-void sendCourt(int val)
+// Flash communication LED
+void flashMyScore(int val)
 {
   if (val == 0) // White
   {
@@ -219,23 +282,180 @@ void sendCourt(int val)
     digitalWrite(commLedPin, LOW);
     delay(300);
   }
-  Serial3.print(val);
-  sendCourtB = false;
 }
 
 // -----------------------------------------------
-// Receive communication
-void receive() {
-  if (Serial3.available()) { // receiving
-    digitalWrite(11, HIGH);
+// Communication
+// Returns answer if done, or -1 if not done yet
+int communication() {
+  // Sending
+  Serial3.print(letters.charAt(myTeam * 3 + myScore));
+  delay(1000);
 
+  // Receiving
+  int maxReceives = 10;
+  int receives = 0;
+  while (Serial3.available() && receives++ < maxReceives) {
     char incoming = Serial3.read();
-    Serial.println(incoming);
+
+    int incomingPos = letters.indexOf(incoming);
+    received[(int) (incomingPos / 3)] = incomingPos % 3;
   }
-  else {
-    digitalWrite(11, LOW);
+
+  // Check to see if done
+  int sum = 0;
+  for (int i = 0; i < sizeof(received) / 2; i++) {
+    if (received[i] == -1) { // Not done, so return -1
+      return -1;
+    }
+    sum += received[i];
+  }
+
+  // Done, so return mod 3 of the results
+  return sum % 3;
+}
+
+// -----------------------------------------------
+// Dance
+void dance() {
+  // Perform dance
+  int beatMillis = 500;
+
+  // Wiggle back
+  for (int i = 0; i < 4; i++) {
+    // Back left
+    servoLeft.writeMicroseconds(1480);
+    servoRight.writeMicroseconds(1650);
+    delay(beatMillis / 2);
+
+    // Back right
+    servoLeft.writeMicroseconds(1350);
+    servoRight.writeMicroseconds(1520);
+    delay(beatMillis / 2);
+  }
+
+  // Forward-back forward-back spin x2
+  for (int i = 0; i < 2; i++) {
+    // Forward-back forward-back
+    for (int j = 0; j < 2; j++) {
+      servoLeft.writeMicroseconds(1700);
+      servoRight.writeMicroseconds(1300);
+      delay((int) (beatMillis * 0.75));
+      servoLeft.writeMicroseconds(1300);
+      servoRight.writeMicroseconds(1700);
+      delay((int) (beatMillis * 0.25));
+    }
+
+    // Spin
+    if (i == 0) { // Spin right then back
+      servoLeft.writeMicroseconds(1700);
+      servoRight.writeMicroseconds(1700);
+      delay(beatMillis);
+      servoLeft.writeMicroseconds(1300);
+      servoRight.writeMicroseconds(1300);
+      delay(beatMillis);
+    }
+    else { // Spin left then back
+      servoLeft.writeMicroseconds(1300);
+      servoRight.writeMicroseconds(1300);
+      delay(beatMillis);
+      servoLeft.writeMicroseconds(1700);
+      servoRight.writeMicroseconds(1700);
+      delay(beatMillis);
+    }
+  }
+
+  // Turn out turn in (each side) x2
+  for (int i = 0; i < 2; i++) {
+    // Left turn out
+    servoLeft.writeMicroseconds(1520);
+    servoRight.writeMicroseconds(1300);
+    delay(beatMillis / 2);
+
+    // Left turn in
+    servoLeft.writeMicroseconds(1480);
+    servoRight.writeMicroseconds(1700);
+    delay(beatMillis / 2);
+
+    // Right turn out
+    servoLeft.writeMicroseconds(1700);
+    servoRight.writeMicroseconds(1480);
+    delay(beatMillis / 2);
+
+    // Right turn in
+    servoLeft.writeMicroseconds(1300);
+    servoRight.writeMicroseconds(1520);
+    delay(beatMillis / 2);
+  }
+
+  // Stop
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500);
+}
+
+// -----------------------------------------------
+// Lightshow
+void lightshowz() {
+  flashall(50);
+  delay(500);
+  flashall(100);
+  delay(500);
+  linear(2);
+  flashall(50);
+  delay(500);
+  flashall(100);
+  delay(500);
+}
+
+void flashall(int timetoflash) {
+  digitalWrite(greenpin, HIGH);  //turn the sent light off
+  digitalWrite(redpin, HIGH);  //turn the sent light off
+  digitalWrite(yellowpin, HIGH);  //turn the sent light off
+  delay(timetoflash);
+  digitalWrite(greenpin, LOW);  //turn the sent light off
+  digitalWrite(redpin, LOW);  //turn the sent light off
+  digitalWrite(yellowpin, LOW);  //turn the sent light off
+}
+
+void linear(int timestolinear) {
+  for (int timez = 0; timez <= timestolinear; timez = timez + 1)
+  {
+    digitalWrite(greenpin, HIGH);  //turn the sent light off
+    delay(150);
+    digitalWrite(greenpin, LOW);
+    digitalWrite(yellowpin, HIGH);  //turn the sent light off
+    delay(150);
+    digitalWrite(yellowpin, LOW);
+    digitalWrite(redpin, HIGH);  //turn the sent light off
+    delay(150);
+    digitalWrite(redpin, LOW);
+    digitalWrite(yellowpin, HIGH);  //turn the sent light off
+    delay(150);
+    digitalWrite(yellowpin, LOW);
+    digitalWrite(greenpin, HIGH);  //turn the sent light off
+    delay(150);
+    digitalWrite(greenpin, LOW);
   }
 }
+
+// -----------------------------------------------
+// National Anthem
+void playAnthem() {
+  // iterate through the notes of the anthem
+  int numberOfNotes = min(sizeof(melodyBass) / 2, sizeof(noteDurations5) / 2);
+  for (int thisNote = 0; thisNote < numberOfNotes/*44*/; thisNote++) {
+
+    int noteDuration = noteDurations5[thisNote] * 250;
+    tone(piezoPin, melodyBass[thisNote], noteDuration);
+
+    // set the time between notes (duration + 30%)
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone
+    noTone(piezoPin);
+  }
+}
+
 
 // -----------------------------------------------
 // Debug via serial
